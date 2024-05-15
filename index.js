68e79332dba460df87967264254a98f71e0a60f1
Zeegaths <zarahgathoni76@gmail.com>
2024-05-15 16:06:33 +0100

    score counter

---
+++ index.js
@@ -0,0 +1,41 @@
+count1 = 0; 
+scores = document.getElementById("scores");
+
+count = 0;
+homeScores = document.getElementById("home-score");
+
+
+function plusOne() {    
+    count1 += 1; 
+    scores.textContent = count1;       
+}
+
+function plusTwo() {    
+    count1 += 2; 
+    scores.textContent = count1;       
+}
+
+function plusThree() {    
+    count1 += 2; 
+    scores.textContent = count1;       
+}
+
+
+function homeOne() {    
+    count += 1; 
+    homeScores.textContent = count;       
+}
+
+function homeTwo() {    
+    count += 2; 
+    homeScores.textContent = count;       
+}
+
+function homeThree() {    
+    count += 2; 
+    homeScores.textContent = count;       
+}
+
+
+
+
